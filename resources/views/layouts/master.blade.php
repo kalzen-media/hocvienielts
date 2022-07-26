7a2f374bedfc700708c225eed551fdc0e912bd48
Mạnh Nguyễn <manhnguyen@kalzen.com>
2022-07-26 15:31:14 +0700

    fix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -151,36 +151,37 @@
     };
     </script>
     @endif
-    <!-- Messenger Plugin chat Code -->
-    <div id="fb-root"></div>
 
-    <!-- Your Plugin chat code -->
-    <div id="fb-customer-chat" class="fb-customerchat">
-    </div>
+ <!-- Messenger Plugin chat Code -->
+ <div id="fb-root"></div>
 
-    <script>
-      var chatbox = document.getElementById('fb-customer-chat');
-      chatbox.setAttribute("page_id", "129367490945559");
-      chatbox.setAttribute("attribution", "biz_inbox");
-    </script>
+ <!-- Your Plugin chat code -->
+ <div id="fb-customer-chat" class="fb-customerchat">
+ </div>
 
-    <!-- Your SDK code -->
-    <script>
-      window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v14.0'
-        });
-      };
+ <script>
+ var chatbox = document.getElementById('fb-customer-chat'  ;
+ chatbox.setAttribute("page_id", "129367490945559"  ;
+ chatbox.setAttribute("attribution", "biz_inbox"  ;
+ </script>
 
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-    </script>
+ <!-- Your SDK code -->
+ <script>
+ window.fbAsyncInit = function() {
+ FB.init({
+ xfbml : true,
+ version : 'v14.0'
+ });
+ };
+
+ (function(d, s, id) {
+ var js, fjs = d.getElementsByTagName(s)[0];
+ if (d.getElementById(id)) return;
+ js = d.createElement(s); js.id = id;
+ js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';;
+ fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'  );
+ </script>
 </body>
 
 </html>
